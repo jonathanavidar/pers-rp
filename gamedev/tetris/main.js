182fcfb1adac30c83ca3b1d2fc033fc9857f4e17
jonathanavidar <jonathanavidar06@gmail.com>
2021-04-21 12:29:40 +0300

    personal update 21/04/21

---
+++ gamedev/tetris/main.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    
+    const grid = document.querySelector(".grid");
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay= document.querySelector('#score');
+    const StartBtn= document.querySelector('#start-button')
+    const width = 10;
+
+    //TETROMINOES
+    const lTetromino = [
+        [1,width+1,width*2+1,2],
+        [width,width+1,width+2,width*2+2],
+        [1,width+1,width*2+1,width*2],
+        [width,width*2,width*2+1,width*2+2]
+    ]
+
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1,width+2,width*2,width*2+1]
+    ]
+
+    const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+    ]
+
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ]
+
+    const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+    ]
+
+    const theTetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
+
+    let currentPosition = 4;
+    let currentRotation = 0;
+    //randomly select a tetromino and its first rotation
+    let random = Math.floor(Math.random()*theTetrominoes.length);
+    
+    let current = theTetrominoes[random][0];
+
+    //Draw the first rotation in the first tetromino
+    function draw(){
+        current.forEach(index =>{
+            squares[currentPosition+index].classList.add('tetromino')
+        })
+    }
+    
+    //undraw the tetromino
+    function undraw(){
+        current.forEach(index =>{
+            squares[currentPosition+index].classList.remove("tetromino");
+        })
+    }
+
+    timerId = setInterval(moveDown, 1000);
+
+    function moveDown(){
+        undraw();
+        currentPosition += width;
+        draw();
+        freeze();
+    }
+
+    function freeze(){
+        if (current.some(index =>squares[currentPosition + index+ width].classList.contains("taken"))){
+            current.forEach(index =>squares[currentPosition + index].classList.add("taken"));
+            //start a new tetronimo falling
+            random = Math.floor(Math.random() * theTetrominoes.lenght);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw();
+        }
+    }
+
+
+})
